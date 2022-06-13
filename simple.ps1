192b72a94927b69ac077f7fe398d2a02841c7549
kaz <31064855+kazro@users.noreply.github.com>
2022-06-14 00:30:06 +0100

    Create simple.ps1

---
+++ simple.ps1
@@ -0,0 +1,51 @@
+$programs = @(
+"Microsoft Edge",
+"Microsoft Edge Update",
+"Microsoft Edge WebView2 Runtime",
+"Cortana",
+"Microsoft News",
+"MSN Weather",
+"Xbox",
+"Get Help",
+"Microsoft Tips",
+"HEIF Image Extensions",
+"Office",
+"Microsoft Solitaire Collection",
+"Microsoft Sticky Notes",
+"Microsoft People",
+"Power Automate",
+"Windows Security",
+"Store Experience Host",
+"Microsoft To Do",
+"VP9 Video Extensions",
+"Web Media Extensions",
+"Webp Image Extensions",
+"Microsoft Photos",
+"Windows Alarms & Clock",
+"Windows Camera",
+"Feedback Hub",
+"Windows Maps",
+"Windows Notepad",
+"Windows Voice Recorder",
+"Xbox TCUI ",
+"Xbox Game Bar Plugin",
+"Xbox Game Bar ",
+"Xbox Identity Provider",
+"Xbox Game Speech Window",
+"Your Phone",
+"Groove Music",
+"Movies & TV ",
+"Microsoft Teams",
+"Windows Web Experience Pack",
+"Mail and Calendar"
+# "App Installer",
+# "Microsoft Store",
+# "Windows Terminal",
+# "Windows Package Manager Source (winget)",
+)
+
+foreach ($i in $programs) {
+    $command = `winget uninstall "$i"`
+    
+    $command
+}
